303a46d02d7dc89b3592c2f609b9292618cda3f6
on15floor <on15floor@gmail.com>
2021-03-23 09:41:20 +0300

    Add get_synonym, get_antonym functions

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
-from random import randint
-from bot_utils import read_token
+
+from bot_utils import read_token, get_coin, get_cube, get_synonym, get_antonym
 
 # Инициализация бота
 bot_token = read_token('token.txt')
@@ -13,47 +13,66 @@ print('[i] Starting bot')
 def command_start(message):
     bot.send_message(message.chat.id, '*Список доступных команд:*\n'
                                       '/coin - Подбросить монетку\n'
-                                      '/cube - Подбросить кубик', parse_mode= 'Markdown')
+                                      '/cube - Подбросить кубик\n'
+                                      '/synonym - Поиск синонимов\n'
+                                      '/antonym - Поиск антонимов', parse_mode= 'Markdown')
 
 
 # Бросить монетку
 @bot.message_handler(commands=['coin'])
 def command_coin(message):
-    orel = randint(0, 1)
-    if orel == 1:
-        bot.send_photo(message.chat.id, 'https://monetka.nurk.ru/images/reshka.png')
-    else:
-        bot.send_photo(message.chat.id, 'https://monetka.nurk.ru/images/orel.png')
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(telebot.types.InlineKeyboardButton(text='Подбросить еще раз 🌕', callback_data='coin'),
                  telebot.types.InlineKeyboardButton(text='К списку команд', callback_data='back'))
+
+    bot.send_photo(message.chat.id, get_coin())
     bot.send_message(message.chat.id, text="Подбросить еще раз монетку?", reply_markup=keyboard)
 
 
 # Бросить кубик
 @bot.message_handler(commands=['cube'])
 def command_cube(message):
-    cub = randint(1, 6)
-    cub_e = ''
-    if cub == 1:
-        cub_e = '1️⃣'
-    elif cub == 2:
-        cub_e = '2️⃣'
-    elif cub == 3:
-        cub_e = '3️⃣'
-    elif cub == 4:
-        cub_e = '4️⃣'
-    elif cub == 5:
-        cub_e = '5️⃣'
-    elif cub == 6:
-        cub_e = '6️⃣'
-    bot.send_message(message.chat.id, cub_e)
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(telebot.types.InlineKeyboardButton(text='Подбросить еще раз 🎲', callback_data='cube'),
                  telebot.types.InlineKeyboardButton(text='К списку команд', callback_data='back'))
+
+    bot.send_message(message.chat.id, get_cube())
     bot.send_message(message.chat.id, text="Подбросить еще раз кубик?", reply_markup=keyboard)
 
 
+# Поиск синонимов (ввод слова)
+@bot.message_handler(commands=['synonym'])
+def command_input_synonym(message):
+    sent = bot.send_message(message.chat.id, 'Введите слово для поиска его синонима:')
+    bot.register_next_step_handler(sent, command_synonym)
+
+
+# Поиск синонимов (возвращение синонима)
+def command_synonym(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(telebot.types.InlineKeyboardButton(text='Начать поиск нового синонима', callback_data='synonym'),
+                 telebot.types.InlineKeyboardButton(text='К списку команд', callback_data='back'))
+
+    bot.send_message(message.chat.id, get_synonym(message.text), reply_markup=keyboard)
+
+
+# Поиск антонимов (ввод слова)
+@bot.message_handler(commands=['antonym'])
+def command_input_antonym(message):
+    sent = bot.send_message(message.chat.id, 'Введите слово для поиска его антонима:')
+    bot.register_next_step_handler(sent, command_antonym)
+
+
+# Поиск антонимов (возвращение синонима)
+def command_antonym(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(telebot.types.InlineKeyboardButton(text='Начать поиск нового антонима', callback_data='antonym'),
+                 telebot.types.InlineKeyboardButton(text='К списку команд', callback_data='back'))
+
+    bot.send_message(message.chat.id, get_antonym(message.text), reply_markup=keyboard)
+
+
+# Обработка результатов
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
     if call.data == 'back':
@@ -62,6 +81,10 @@ def query_handler(call):
         command_coin(call.message)
     elif call.data == 'cube':
         command_cube(call.message)
+    elif call.data == 'synonym':
+        command_input_synonym(call.message)
+    elif call.data == 'antonym':
+        command_input_antonym(call.message)
     # Скрыть клавиатуру
     bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
 
--- bot_utils.py
+++ bot_utils.py
@@ -1,4 +1,59 @@
+from random import randint
+from bs4 import *
+import requests
+
+
 # Считывание токена из файла
 def read_token(filename: str):
     with open(filename) as f:
         return f.read().strip()
+
+
+# Возвращает ссылку на случайную монету
+def get_coin():
+    orel = randint(0, 1)
+    if orel == 1:
+        link = 'https://monetka.nurk.ru/images/reshka.png'
+    else:
+        link = 'https://monetka.nurk.ru/images/orel.png'
+    return link
+
+
+# Возвращает случайное значение кубика
+def get_cube():
+    cub = randint(1, 6)
+    if cub == 1:
+        cub_e = '1️⃣'
+    elif cub == 2:
+        cub_e = '2️⃣'
+    elif cub == 3:
+        cub_e = '3️⃣'
+    elif cub == 4:
+        cub_e = '4️⃣'
+    elif cub == 5:
+        cub_e = '5️⃣'
+    else:
+        cub_e = '6️⃣'
+    return cub_e
+
+
+# Возвращает синоним к слову
+def get_synonym(word: str):
+    response = requests.get(f"https://synonymonline.ru/{word[0].title()}/{word.lower().strip()}")
+    html = BeautifulSoup(''.join(response.text), features="html.parser")
+    class_synonym = html.findAll("ol", {'class': 'synonyms-list row'})
+    if class_synonym:
+        return class_synonym[0].text.replace('\n', ' | ')
+    else:
+        return 'По вашему слову синонимов не найдено :('
+
+
+# Возвращает антоним к слову
+def get_antonym(word: str):
+    response = requests.get(f"https://antonymonline.ru/{word[0].title()}/{word.lower().strip()}")
+    html = BeautifulSoup(''.join(response.text), features="html.parser")
+    class_synonym = html.findAll("ol", {'class': 'row antonyms-list'})
+    if class_synonym:
+        return class_synonym[0].text.replace('\n', ' | ')
+    else:
+        return 'По вашему слову антонимов не найдено :('
